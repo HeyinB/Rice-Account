52a88f414c5151ea1fa1f172b51ee78d702a63d7
HeyinB <3164249018@qq.com>
2022-08-18 15:41:20 +0800

    修改登录api，删除云函数

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,5 +1,5 @@
 const app = getApp()
-import vxCloud from '../../utils/vxCloud'
+import { loginReq } from '../../http/login'
 Page({
     data: {
         userInfo: {}
@@ -12,31 +12,65 @@ Page({
     getUserInfo() {
         wx.getUserProfile({
             desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-            success: async(res) => {
+            success: async (user) => {
+                let { userInfo } = user
+
+                wx.login({
+                    async success(res) {
+                        userInfo.code = res.code
+
+                        let data = await loginReq(userInfo)
+                        console.log('-------data---------', data);
+                        await wx.setStorage({
+                            key: "userInfo",
+                            data,
+                            encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
+                            success(res) {
+                                console.log('------setStorage---------', res);
+                            }
+                        })
+
+
+                        await wx.getStorage({
+                            key: "userInfo",
+                            encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
+                            success(res) {
+                                console.log('------getStorage---------', res);
+                            }
+                        })
+
+                    }
+                })
                 // 本地储存
-                res.userInfo.loginData = new Date()
+                // res.userInfo.loginData = new Date()
 
-                let { result } = await vxCloud('initLedger', res.userInfo)
+                // let { result } = await require({
+                //     url: 'login/LoginOrRegister',
+                //     method: 'post',
+                //     data: {
+                //         ...res
+                //     }
+                // })
 
-                if (result.code == 200) {
-                    wx.setStorage({
-                        key: "userInfo",
-                        data: res.userInfo,
-                        encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
-                        success() {
-                            wx.getStorage({
-                                key: "userInfo",
-                                encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
-                                success(res) {
-                                    getApp().globalData.userInfo = res.data
-                                    wx.reLaunch({
-                                        url: '../ledger/ledger'
-                                    })
-                                }
-                            })
-                        }
-                    })
-                }
+                // if (result.code == 200) {
+                //     wx.setStorage({
+                //         key: "userInfo",
+                //         data: res.userInfo,
+                //         encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
+                //         success() {
+                //             wx.getStorage({
+                //                 key: "userInfo",
+                //                 encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
+                //                 success(res) {
+                //                     getApp().globalData.userInfo = res.data
+                //                     wx.reLaunch({
+                //                         url: '../ledger/ledger'
+                //                     })
+                //                 }
+                //             })
+                //         }
+                //     })
+                // }
 
             }
         })
