8238878cf42653a4d98a827a52cabac01078e155
HeyinB <3164249018@qq.com>
2022-08-19 17:56:15 +0800

    登录

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -27,17 +27,21 @@ Page({
                             encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
                             success(res) {
                                 console.log('------setStorage---------', res);
+                                getApp().globalData.userInfo = res.data
+                                wx.reLaunch({
+                                    url: '../ledger/ledger'
+                                })
                             }
                         })
 
 
-                        await wx.getStorage({
-                            key: "userInfo",
-                            encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
-                            success(res) {
-                                console.log('------getStorage---------', res);
-                            }
-                        })
+                        // await wx.getStorage({
+                        //     key: "userInfo",
+                        //     encrypt: true, // 若开启加密存储，setStorage 和 getStorage 需要同时声明 encrypt 的值为 true
+                        //     success(res) {
+                        //         console.log('------getStorage---------', res);
+                        //     }
+                        // })
 
                     }
                 })
