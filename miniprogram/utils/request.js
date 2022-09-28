6c9b3729e0ef3aefbec120ebf5ee62bfa9a27e72
HeyinB <3164249018@qq.com>
2022-09-28 15:47:32 +0800

    api修改

--- miniprogram/utils/request.js
+++ miniprogram/utils/request.js
@@ -1,77 +1,82 @@
 
-import {getStorageFun,setStorageFun} from './storageFun'
+import { getStorageFun, setStorageFun } from './storageFun'
 
-// prourl = 'http://47.97.214.217:5000/v1/'
-// http://localhost:5000/v1/
-const baseUrl = "http://localhost:5000/v1/";
+let api = {
+    pro: 'https://api.heyinb.xyz/v1/',
+    env: 'http://localhost:5000/v1/'
+}
+
+
+const baseUrl = api.pro
+
 let temp_request = [],
-  is_freshing = false;
+    is_freshing = false;
 
 async function require({ url, data, method = "GET" }) {
 
-  url = baseUrl + url;
-  let params_ = arguments;
-  let userInfo = await getStorageFun('userInfo') 
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url,
-      data,
-      method,
-      header: {
-        "content-type": "application/json", // 默认值
-        authorization: userInfo?userInfo.token:''
-      },
-      async success(res) {
-        // if(data.code === 200) resolve(res)
-        switch (res.data.code) {
-          case 200:
-            resolve(res);
-            break;
-          case 401:
-            if (!is_freshing) {
-              //刷新token
-              await refresh();
-            }
-            console.log('token过期了')
-            resolve(
-              new Promise((reslove) => {
-                temp_request.push(() => {
-                  reslove(require(...params_));
-                });
-              })
-            );
-            break;
-          default:
-            reject(res.data);
-        }
-      },
-      fail(res) {
-        reject(res);
-      },
+    url = baseUrl + url;
+    let params_ = arguments;
+    let userInfo = await getStorageFun('userInfo')
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url,
+            data,
+            method,
+            header: {
+                "content-type": "application/json", // 默认值
+                authorization: userInfo ? userInfo.token : ''
+            },
+            async success(res) {
+                // if(data.code === 200) resolve(res)
+                switch (res.data.code) {
+                    case 200:
+                        resolve(res);
+                        break;
+                    case 401:
+                        if (!is_freshing) {
+                            //刷新token
+                            await refresh();
+                        }
+                        console.log('token过期了')
+                        resolve(
+                            new Promise((reslove) => {
+                                temp_request.push(() => {
+                                    reslove(require(...params_));
+                                });
+                            })
+                        );
+                        break;
+                    default:
+                        reject(res.data);
+                }
+            },
+            fail(res) {
+                reject(res);
+            },
+        });
     });
-  });
 }
 
 async function refresh() {
-  is_freshing = true;
-  wx.login({
-    async success(res) {
+    is_freshing = true;
+    wx.login({
+        async success(res) {
 
-      let userInfo = await getStorageFun('userInfo')
+            let userInfo = await getStorageFun('userInfo')
 
-      let { data } = await require({
-        url: "login/refreshToken",
-        method: "POST",
-        data: { id: userInfo.id },
-      });
+            let { data } = await require({
+                url: "login/refreshToken",
+                method: "POST",
+                data: { id: userInfo.id },
+            });
 
-      await setStorageFun("userInfo",data.data)
-      is_freshing = false;
-      temp_request.map((cb) => cb());
-      // 清空temp_request
-      temp_request = [];
-    },
-  });
+            await setStorageFun("userInfo", data.data)
+            is_freshing = false;
+            temp_request.map((cb) => cb());
+            // 清空temp_request
+            temp_request = [];
+        },
+    });
 }
 
 export default require;
